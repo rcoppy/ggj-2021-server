76f49a37a886bfc49d517ac00936e33b7967d482
Alex Rupp-Coppi <amr2311@columbia.edu>
2021-01-31 14:16:18 -0500

    preliminary wall placement

--- src/Commands.ts
+++ src/Commands.ts
@@ -1,11 +1,11 @@
 // OnJoinCommand.ts
 import { Command } from "@colyseus/command";
 
-import { Coordinate, MyRoomState, Hex, PlayerState, PlayerPieceFactory } from "./rooms/schema/MyRoomState";
+import { Coordinate, MyRoomState, Hex, PlayerState, PlayerPieceFactory, TileOccupant } from "./rooms/schema/MyRoomState";
 
 import { IntCoordinate } from "./IntCoordinate";
 import { PathfindingLogic } from "./PathfindingLogic"; 
-import { Constants } from "./Constants";
+import { Constants, OccupantTypes } from "./Constants";
 
 export class OnJoinCommand extends Command<MyRoomState, { sessionId: string }> {
 
@@ -42,7 +42,54 @@ export class OnLeaveCommand extends Command<MyRoomState, { sessionId: string }> 
 
 export class InitGridCommand extends Command<MyRoomState, { board: Array<{}> }> {
 
+  // very slow method unfortunately
+  searchForTileByCoordinate(x: number, y: number): Hex {
+    for (let pair of this.state.grid) { 
+      if (pair[1].x == x && pair[1].y == y) {
+        return pair[1];   
+      }      
+    };
+
+    return null;
+  }
+
+  spawnWall(startHex: Hex, endHex: Hex) {
+    if (!startHex.isOccupied && !endHex.isOccupied) {
+      
+      const finder = new PathfindingLogic({ width: Constants.gridWidth, height: Constants.gridHeight}); 
+
+      const obstructions = new Array<IntCoordinate>();
+
+      this.state.grid.forEach((hex) => { hex.isOccupied ? obstructions.push(new IntCoordinate(hex.x, hex.y)) : null });
+
+      finder.updateObstructions(obstructions);
+
+      const rawPath: Array<IntCoordinate> = finder.generateMovementQueue({ startX: startHex.x, 
+                                                                            startY: startHex.y, 
+                                                                            endX: endHex.x, 
+                                                                            endY: endHex.y });
+
+      // const positionQueue: Queue<IntCoordinate> = new Queue<IntCoordinate>();
+      
+      rawPath.forEach(coord => {
+        const tile = this.searchForTileByCoordinate(coord.x, coord.y);
+
+        if (tile != null) {
+          const wall = new TileOccupant({tileId: tile.id.toString(), occupantTypeId: OccupantTypes.Wall });
+          tile.isOccupied = true;
+          this.state.tileOccupants.set(tile.id.toString(), wall);
+        }
+        
+      });
+
+      this.state.triggerAll();
+    } else {
+      // send some kind of message to player that request was invalid
+    }  
+  }
+
   execute({ board }) {
+
     board.forEach(hex => {
       const tile: Hex = new Hex({ x: hex.x, y: hex.y})
       this.state.grid.set(`${tile.id}`, tile);
